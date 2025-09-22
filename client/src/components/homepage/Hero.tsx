3c9b13db0e602d04129978d016173f47a244df57
Alfikri <alfzein19@gmail.com>
2025-09-23 00:19:50 +0700

    plz

--- client/src/components/homepage/Hero.tsx
+++ client/src/components/homepage/Hero.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import resumeIllustration from "../../../public/image/heroillus.png";
 import Link from "next/link";
+import resumeIllustration from "../../../public/image/heroillus.png";
 
 const Hero = () => {
   return (
